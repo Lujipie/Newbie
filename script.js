286c54adbf5d359e010dbb125de06af130322393
Glitch (glitch-blank-website) <none>
2024-03-23 20:28:41 +0000

    🍗🐠 Checkpoint ./clubs.html:66178343/173 ./index.html:66179221/249 ./calendar.html:66178321/51 ./script.js:66178343/416

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
+const welcomeText = "Welcome UTD Freshman!";
+const welcomeDiv = document.getElementById("welcome-text");
 
-console.log("Hello, world!");
+function typeWriter(text, i) {
+    if (i < text.length) {
+        welcomeDiv.innerHTML += text.charAt(i);
+        i++;
+        setTimeout(function() {
+            typeWriter(text, i);
+        }, 100); // Adjust the typing speed (milliseconds)
+    }
+}
+
+typeWriter(welcomeText, 0);
